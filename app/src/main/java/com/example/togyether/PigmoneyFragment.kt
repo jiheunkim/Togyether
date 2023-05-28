43b54b8ab2c6e16500225bc77494ccd42845e294
jiheunkim <ziheun54@gmail.com>
2023-05-29 03:35:46 +0900

    가계부 화면 서브 메뉴(달력, 내역, 예산) 추가

--- app/src/main/java/com/example/togyether/PigmoneyFragment.kt
+++ app/src/main/java/com/example/togyether/PigmoneyFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.togyether.databinding.FragmentPigmoneyBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +22,7 @@ class PigmoneyFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    lateinit var binding: FragmentPigmoneyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +35,43 @@ class PigmoneyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pigmoney, container, false)
+        binding = FragmentPigmoneyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subMenu()
+    }
+
+    private fun subMenu() {
+        val subBtn = binding.pigmoneyMenu
+
+        subBtn.run {
+            setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.first -> {
+                        // 다른 프래그먼트 화면으로 이동하는 기능
+                        val calenderFragment = CalenderFragment()
+                        childFragmentManager.beginTransaction()
+                            .replace(R.id.sub_container, calenderFragment).commit()
+                    }
+                    R.id.second -> {
+                        val contentFragment = ContentFragment()
+                        childFragmentManager.beginTransaction()
+                            .replace(R.id.sub_container, contentFragment).commit()
+                    }
+                    R.id.third -> {
+                        val budgetFragment = BudgetFragment()
+                        childFragmentManager.beginTransaction()
+                            .replace(R.id.sub_container, budgetFragment).commit()
+                    }
+
+                }
+                true
+            }
+            selectedItemId = R.id.first
+        }
     }
 
     companion object {
