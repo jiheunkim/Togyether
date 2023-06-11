294ee162ede9c6afba73a043eb62e6fe5bf7d360
jiheunkim <ziheun54@gmail.com>
2023-06-11 14:57:42 +0900

    로그인 수정 (아이디,비밀번호 정보 저장)

--- app/src/main/java/com/example/togyether/LoginActivity.kt
+++ app/src/main/java/com/example/togyether/LoginActivity.kt
@@ -1,62 +1,112 @@
 package com.example.togyether
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.togyether.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
-    private lateinit var auth: FirebaseAuth
+    lateinit var auth: FirebaseAuth
+    lateinit var btn_signUp: Button
+    lateinit var btn_signIn: Button
+    lateinit var edt_email: EditText
+    lateinit var edt_password: EditText
+    lateinit var preference: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        initLayout()
+        initProperty()
+        initializeView()
+        init()
     }
 
-    private fun initLayout() {
-        binding.apply {
-            loginBtn.setOnClickListener {
-                val id = loginId.text.toString()
-                val passwd = loginPasswd.text.toString()
+    private fun init() {
+        val id = binding.loginId.text.toString()
+        val passwd = binding.loginPasswd.text.toString()
 
-                auth = Firebase.auth
+        //로그인 버튼
+        binding.loginBtn.setOnClickListener {
+            login(id, passwd)
+        }
 
-                if (id.isEmpty() && passwd.isEmpty()) {
-                    Toast.makeText(this@LoginActivity, "아이디와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
-                } else if (id.isEmpty()) {
-                    Toast.makeText(this@LoginActivity, "아이디를 입력해주세요.", Toast.LENGTH_SHORT).show()
-                } else if (passwd.isEmpty()) {
-                    Toast.makeText(this@LoginActivity, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
-                } else {
-                    auth.signInWithEmailAndPassword(id, passwd)
-                        .addOnCompleteListener(this@LoginActivity) { task ->
-                            if (task.isSuccessful) {
-                                // Sign in success, update UI with the signed-in user's information
+        //회원가입 화면 인텐트
+        binding.joinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
 
-                                //데이터베이스에 user정보를 넣어줘야함
-                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                startActivity(intent)
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                //updateUI(null)
-                                Toast.makeText(this@LoginActivity, "login 실패", Toast.LENGTH_LONG).show()
-                            }
-                        }
+        //아이디 찾기 화면 인텐트
+        binding.findIdBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //비밀번호 찾기 화면 인텐트
+        binding.resetPasswdBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun login(id: String, passwd: String) {
+        //아이디 또는 패스워드가 입력되었는지 유효성 검사
+        if (edt_email.text.toString().isNullOrBlank() || edt_password.text.toString().isNullOrBlank())
+            Toast.makeText(this, "아이디 또는 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+        else {
+            //로그인 실행
+            auth.signInWithEmailAndPassword(edt_email.text.toString(), edt_password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("로그인", "성공")
+                        val user = auth.currentUser
+                        updateUI(user)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                    }
                 }
-            }
+        }
+    }
 
-            joinBtn.setOnClickListener {
-                val intent = Intent(this@LoginActivity, JoinActivity::class.java)
+    fun initProperty() {    //초기 변수 세팅
+        auth = FirebaseAuth.getInstance()     //Firebase 계정 관련 변수
+        preference = getSharedPreferences("setting", MODE_PRIVATE)    //로그인 정보 저장용 SharedPreference
+    }
+
+    fun initializeView() {   //뷰 초기화
+        edt_email = binding.loginId
+        edt_password = binding.loginPasswd
+
+        edt_email.setText(preference.getString("email", ""))     //마지막으로 로그인 한 이메일 세팅
+        edt_password.setText(preference.getString("password", ""))   //마지막으로 로그인 한 패스워드 세팅
+    }
+
+    private fun updateUI(user: FirebaseUser?) { //로그인 성공 시 화면 이동
+        if (user != null) {
+            try {
+                var preference = getSharedPreferences("setting", MODE_PRIVATE).edit()    //이메일 및 패스워드 저장
+                preference.putString("email", edt_email.text.toString())
+                preference.putString("password", edt_password.text.toString())
+                preference.apply()
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
+                finish()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
             }
         }
     }
