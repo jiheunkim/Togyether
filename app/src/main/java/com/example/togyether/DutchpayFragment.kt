1208bda6205d359f261503c9506c373cb69e87d5
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2023-06-13 15:09:09 +0900

    프래그먼트간 데이터 통신 완료

--- app/src/main/java/com/example/togyether/DutchpayFragment.kt
+++ app/src/main/java/com/example/togyether/DutchpayFragment.kt
@@ -8,16 +8,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentResultListener
 import com.example.togyether.databinding.FragmentDutchpayBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class DutchpayFragment : Fragment() {
     lateinit var binding: FragmentDutchpayBinding
     lateinit var dActivity: MainActivity
-    val fragmentList = ArrayList<Fragment>()
     lateinit var adapter: DutchpayPageAdapter
+
+    companion object Static{
+        var groupSize = 0
+        val groupMemberList = ArrayList<ArrayList<String>>()
+        val groupNameList = ArrayList<String>()
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -35,34 +42,44 @@ class DutchpayFragment : Fragment() {
             binding.viewPager.adapter = adapter
             TabLayoutMediator(binding.tabLayout, binding.viewPager){
                     tab , pos ->
-                tab.text = "GROUP${pos + 1}"
+                tab.text = groupNameList[pos]
             }.attach()
         }
 
         // arguments가 null이면 블록 내부로 진입 X
+        Log.d("size", arguments?.size().toString())
+//        if(arguments == null){
+//            Toast.makeText(requireContext(), "arguments null", Toast.LENGTH_SHORT).show()
+//        }
         arguments?.let {
+            // 새로 추가할 그룹 정보 수신
             val groupName = it.getString("group_name")!!
-            adapter.addFragment(groupName)
+//            Toast.makeText(requireContext(), groupName, Toast.LENGTH_SHORT).show()
             val groupMembers = ArrayList<String>()
-            for(i in 1 .. it.size()){
+            for(i in 1 until it.size()){
                 val key = "member${i}"
                 it.getString(key)?.let { member -> groupMembers.add(member) }
-                Log.d("member", groupMembers[i-1])
             }
+//            for(i in groupMembers){
+//                Log.d("check_member$i", i)
+//            }
+
+            // 기존 그룹 + 신규 그룹 Adapter에 추가
+            groupSize++
+            groupNameList.add(groupName)
+            groupMemberList.add(groupMembers)
+            for(i in 0 until groupSize){
+                adapter.addFragment(groupNameList[i], groupMemberList[i])
+            }
+
             adapter.notifyDataSetChanged()
         }
 
         binding.addBtn.setOnClickListener(){
             // DutchpayFragment -> AddGroupFragment
             parentFragmentManager.beginTransaction()
-                .hide(this)
-                .add(R.id.root_fragment, AddGroupFragment())
-                .addToBackStack(null)
+                .replace(R.id.root_fragment, AddGroupFragment())
                 .commit()
-
-
-            adapter.addFragment("1111")
-            adapter.notifyDataSetChanged()
         }
         return view
     }
