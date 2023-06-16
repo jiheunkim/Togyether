d298ea9e77fc71164807270e98b6c5e957452cfc
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2023-06-16 17:09:15 +0900

    memberData 객체 사용

--- app/src/main/java/com/example/togyether/spendingData.kt
+++ app/src/main/java/com/example/togyether/spendingData.kt
@@ -1,3 +1,3 @@
 package com.example.togyether
 
-class spendingData (var title:String, var time:String, var name:String, var amount:Int, var group:ArrayList<String>)
+class spendingData (var title:String, var time:String, var amount:Int, var name:String, var group:ArrayList<memberData>)
