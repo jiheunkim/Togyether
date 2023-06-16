188d9f337724e72fb15a1932361da097a5f62c9d
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2023-06-16 17:10:58 +0900

    memberData 객체 사용

--- app/src/main/java/com/example/togyether/DutchpayGroupFrameFragment.kt
+++ app/src/main/java/com/example/togyether/DutchpayGroupFrameFragment.kt
@@ -1,40 +1,40 @@
-package com.example.togyether
+    package com.example.togyether
 
-import android.content.Context
-import android.os.Bundle
-import android.text.TextUtils.replace
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.togyether.databinding.FragmentDutchpayFrameBinding
-import com.example.togyether.databinding.FragmentDutchpayGroupFrameBinding
+    import android.content.Context
+    import android.os.Bundle
+    import android.text.TextUtils.replace
+    import android.view.LayoutInflater
+    import android.view.View
+    import android.view.ViewGroup
+    import androidx.fragment.app.Fragment
+    import com.example.togyether.databinding.FragmentDutchpayFrameBinding
+    import com.example.togyether.databinding.FragmentDutchpayGroupFrameBinding
 
 
-class DutchpayGroupFrameFragment(var nameList: ArrayList<String>) : Fragment() {
-    lateinit var binding: FragmentDutchpayGroupFrameBinding
-    lateinit var dActivity: MainActivity
+    class DutchpayGroupFrameFragment(var memberList: ArrayList<memberData>) : Fragment() {
+        lateinit var binding: FragmentDutchpayGroupFrameBinding
+        lateinit var dActivity: MainActivity
 
-    var dutchpayGroupFragment = DutchpayGroupFragment(nameList)
+        var dutchpayGroupFragment = DutchpayGroupFragment(memberList)
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        dActivity = context as MainActivity
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentDutchpayGroupFrameBinding.inflate(layoutInflater, container, false)
-        val view = binding.root
-        with(view) {
-            childFragmentManager.beginTransaction().apply {
-                replace(R.id.root_fragment, dutchpayGroupFragment)
-                commit()
-            }
+        override fun onAttach(context: Context) {
+            super.onAttach(context)
+            dActivity = context as MainActivity
         }
-        return view
-    }
 
-}
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            binding = FragmentDutchpayGroupFrameBinding.inflate(layoutInflater, container, false)
+            val view = binding.root
+            with(view) {
+                childFragmentManager.beginTransaction().apply {
+                    replace(R.id.root_fragment, dutchpayGroupFragment)
+                    commit()
+                }
+            }
+            return view
+        }
+
+    }
