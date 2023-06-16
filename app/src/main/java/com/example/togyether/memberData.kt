c020247409341c481fad91ad87f9f2c851f0d68e
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2023-06-17 02:36:08 +0900

    결산 화면까지

--- app/src/main/java/com/example/togyether/memberData.kt
+++ app/src/main/java/com/example/togyether/memberData.kt
@@ -1,3 +1,3 @@
 package com.example.togyether
 
-class memberData(var name:String, var spend: Int, var transfer: ArrayList<Int>)
+class memberData(var name:String, var num:Int, var spend: Long, var transfer: ArrayList<Long>)
