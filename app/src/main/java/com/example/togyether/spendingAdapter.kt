c0f9055fa2dd4e28de8e1b0dd451268c7ea15c6f
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2023-06-17 03:55:22 +0900

    Update spendingAdapter.kt

--- app/src/main/java/com/example/togyether/spendingAdapter.kt
+++ app/src/main/java/com/example/togyether/spendingAdapter.kt
@@ -14,9 +14,8 @@ import com.example.togyether.databinding.RowSpendingBinding
 class spendingAdapter (var items:ArrayList<spendingData>, var groupNum:Int, var displayMetrics:DisplayMetrics)
     : RecyclerView.Adapter<spendingAdapter.ViewHolder>() {
 
-    val colorList = arrayListOf<String>("#E71D36","#5A9367","#EFDC05","#9055A2","#4F86C6","#f199bc",
-        "#C16200","#FDD692","#00b9f1","#548687","#8F2D56","#F16B6F","#00dffc","#cbe86b","#ee2560","#1a1a1a","#4f953b",
-        "#f4f7f7","#353866","#1F6E8C")
+    val colorList = arrayListOf<String>("#0E2954","#1F6E8C","#2E8A99","#84A7A1","#CEEDC7","#FFF6BD","#FFD4B2","#FCC8D1","#FFABAB","#D14D72","#9A1663","#CD104D"
+        ,"#E14D2A","#FD841F","#FFCC29","#81B214","#206A5D","#1F6E8C","#0A4D68","#00337C")
 
     interface OnItemClickListener{
         fun OnItemClick(data: spendingData, position: Int)
@@ -45,12 +44,12 @@ class spendingAdapter (var items:ArrayList<spendingData>, var groupNum:Int, var 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.title.text=items[position].title
         holder.binding.time.text=items[position].time
-        holder.binding.spender.setBackgroundColor(Color.parseColor(colorList[items[position].num % memberListList[groupNum].size]))
+        holder.binding.spender.setBackgroundColor(Color.parseColor(colorList[(items[position].num + 8 * groupNum) % 20]))
         holder.binding.name.text=memberListList[groupNum][items[position].num].name
         holder.binding.amount.text=items[position].amount.toString()
         for(i in items[position].group){
             val btn = AppCompatButton(holder.binding.spendingMemberIcon.context)
-            btn.setBackgroundColor(Color.parseColor(colorList[i.num % memberListList[groupNum].size]))
+            btn.setBackgroundColor(Color.parseColor(colorList[(i.num + 8 * groupNum) % 20]))
             val param = LinearLayout.LayoutParams(changeDP(18),changeDP(18))
             param.setMargins(changeDP(5), 0, 0 ,0)
             param.gravity=Gravity.CENTER_VERTICAL
