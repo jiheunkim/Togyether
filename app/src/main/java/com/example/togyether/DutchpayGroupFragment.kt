bd22f49854ad43e6ce3fe405bd7abe66c3652fd2
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2023-06-17 02:33:05 +0900

    결산 화면까지

--- app/src/main/java/com/example/togyether/DutchpayGroupFragment.kt
+++ app/src/main/java/com/example/togyether/DutchpayGroupFragment.kt
@@ -11,26 +11,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.togyether.DutchpayFragment.Static.memberListList
 import com.example.togyether.DutchpayFragment.Static.spendingListList
 import com.example.togyether.databinding.FragmentDutchpayGroupBinding
+import com.github.mikephil.charting.utils.Utils.init
 
 class DutchpayGroupFragment(var groupNum:Int) : Fragment() {
     lateinit var binding: FragmentDutchpayGroupBinding
     lateinit var memberAdapter: memberAdapter
     lateinit var spendingAdapter: spendingAdapter
     lateinit var transferAdapter: transferAdapter
-
-    var transferList = ArrayList<transferData>();
-
-    companion object Static{
-        var spendCnt = 0
-        //var spendingList = ArrayList<spendingData>();
-
-
-        var spendingTitleList = ArrayList<String>()
-        var timeList = ArrayList<String>()
-        var amountList = ArrayList<Int>()
-        val spendingNameList = ArrayList<String>()
-        val spendingMemberList = ArrayList<ArrayList<memberData>>()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,8 +36,8 @@ class DutchpayGroupFragment(var groupNum:Int) : Fragment() {
             binding.recyclerView3.addItemDecoration(DividerItemDecoration(getContext(),LinearLayoutManager.VERTICAL))
 
             memberAdapter = memberAdapter(ArrayList())
-            spendingAdapter = spendingAdapter(ArrayList())
-            transferAdapter = transferAdapter(ArrayList())
+            spendingAdapter = spendingAdapter(ArrayList(), groupNum, resources.displayMetrics)
+            transferAdapter = transferAdapter(ArrayList(), groupNum)
 
             binding.recyclerView1.adapter = memberAdapter
             binding.recyclerView2.adapter = spendingAdapter
@@ -62,21 +49,47 @@ class DutchpayGroupFragment(var groupNum:Int) : Fragment() {
             // 새로 추가할 그룹 정보 수신
             val spendingTitle = it.getString("spendingTitle")!!
             val spendingTime = it.getString("spendingTime")!!
-            val spendingAmount = it.getInt("spendingAmount")!!
-            val spendingName = it.getString("spendingName")!!
-
+            val spendingAmount = it.getLong("spendingAmount")!!
+            val spendingNameNum = it.getInt("spendingName")!!
+            val spendingName = memberListList[groupNum][spendingNameNum].name
+            val spendingCnt = it.getInt("spendingCnt")
             val spendingMembers = ArrayList<memberData>()
             for(i in 1 .. memberListList[groupNum].size){
                 val key = "member${i}"
-                it.getInt(key)?.let {
-                        memberNum -> if(i==memberNum){ spendingMembers.add(memberListList[groupNum][memberNum-1])}
+                it.getInt(key)?.let { memberNum ->
+                    if(i==memberNum){
+                        spendingMembers.add(memberListList[groupNum][memberNum-1])
+                        if(memberNum - 1 != spendingNameNum){
+                            memberListList[groupNum][memberNum - 1].transfer[spendingNameNum] += spendingAmount/spendingCnt
+                            Log.i("tranfer1", memberListList[groupNum][memberNum - 1].name + "->"
+                                    + memberListList[groupNum][spendingNameNum].name
+                                    + " " + (spendingAmount/spendingCnt).toString())
+                        }
+                    }
+                }
+            }
+
+            for(i in memberListList[groupNum]){
+                for(j in 0..memberListList[groupNum].size - 1){
+                    Log.i("trnsfr", i.name + "->" + memberListList[groupNum][j].name + " " + i.transfer[j].toString())
                 }
 
             }
 
+            transferAdapter.items.clear()
+            for(i in memberListList[groupNum]){
+                for(j in memberListList[groupNum]){
+                    if(i.transfer[j.num]>j.transfer[i.num]){
+                        transferAdapter.items.add(transferData(i.num, j.num, i.transfer[j.num]-j.transfer[i.num]))
+                        Log.i("transfer2", i.name + " " + j.name +" "+i.transfer[j.num].toString() + " " + j.transfer[i.num].toString())
+                        Log.i("transfer2", i.name + "->" + j.name +" "+ (i.transfer[j.num]-j.transfer[i.num]).toString())
+                    }
+                }
+            }
+            transferAdapter.notifyDataSetChanged()
+
             // 기존 그룹 + 신규 그룹 Adapter에 추가
-            spendCnt++
-            spendingListList[groupNum].add(spendingData(spendingTitle, spendingTime, spendingAmount, spendingName, spendingMembers))
+            spendingListList[groupNum].add(spendingData(spendingTitle, spendingTime, spendingAmount, spendingNameNum, spendingMembers))
             spendingAdapter.items = spendingListList[groupNum]
             spendingAdapter.notifyDataSetChanged()
 
@@ -86,6 +99,13 @@ class DutchpayGroupFragment(var groupNum:Int) : Fragment() {
             // DutchpayFragment -> AddGroupFragment
             parentFragmentManager.beginTransaction()
                 .replace(R.id.root_fragment, AddSpendingFragment(groupNum))
+                .commit()
+        }
+
+        binding.settlementBtn.setOnClickListener(){
+            // DutchpayFragment -> DutchpayTransferFragment
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.root_fragment, DutchpayTransferFragment(groupNum))
                 .commit()
         }
 
@@ -102,6 +122,18 @@ class DutchpayGroupFragment(var groupNum:Int) : Fragment() {
 
         spendingAdapter.items = spendingListList[groupNum]
         spendingAdapter.notifyDataSetChanged()
+
+        transferAdapter.items.clear()
+        for(i in memberListList[groupNum]){
+            for(j in memberListList[groupNum]){
+                if(i.transfer[j.num]>j.transfer[i.num]){
+                    transferAdapter.items.add(transferData(i.num, j.num, i.transfer[j.num]-j.transfer[i.num]))
+                    Log.i("transfer2", i.name + " " + j.name +" "+i.transfer[j.num].toString() + " " + j.transfer[i.num].toString())
+                    Log.i("transfer2", i.name + "->" + j.name +" "+ (i.transfer[j.num]-j.transfer[i.num]).toString())
+                }
+            }
+        }
+        transferAdapter.notifyDataSetChanged()
     }
 }
 
