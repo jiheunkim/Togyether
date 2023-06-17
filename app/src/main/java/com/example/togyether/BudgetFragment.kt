89548ecf65a2624be8904a7c52c7908c54774b01
WonJun-62 <132991365+WonJun-62@users.noreply.github.com>
2023-06-17 17:28:46 +0900

    firebase 연동 (차트 부분 수정필요)

--- app/src/main/java/com/example/togyether/BudgetFragment.kt
+++ app/src/main/java/com/example/togyether/BudgetFragment.kt
@@ -7,34 +7,76 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.togyether.databinding.FragmentBudgetBinding
 import android.app.AlertDialog
+import android.graphics.Color
 import android.text.InputType
+import android.util.Log
 import android.widget.EditText
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.text.NumberFormat
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.util.*
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class BudgetFragment : Fragment() {
 
     private var _binding: FragmentBudgetBinding? = null
     private val binding get() = _binding!!
 
-    private var currentMonth: Int = 5 // 현재 월
     private var budget: Int = 0 // 예산
     private var expenses: Int = 0 // 지출
+    private var income: Int = 0 // 수입
+    lateinit var selectedDate: LocalDate
+    private lateinit var expenseChart: BarChart
+
+    lateinit var myUid: String
+    lateinit var setMonth: String
+    lateinit var getMonth: String
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentBudgetBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        updateMonthView()
+        // 그래프를 위한 BarChart 인스턴스 생성
+        expenseChart = binding.expenseChart
+
         updateExpenseRatio()
 
-        // 월 이동 버튼 클릭 이벤트 처리
+        usernameFromFirebase()
+        calendarFromFirebase()
+
+
+
+        selectedDate = LocalDate.now() // 현재 날짜
+        setMonth = selectedDate.month.toString()
+        setMonthView() // 월 이동
+
         binding.lastMonth.setOnClickListener {
-            currentMonth--
-            updateMonthView()
+            // 이전 달 버튼 이벤트
+            selectedDate = selectedDate.minusMonths(1)
+            setMonth = selectedDate.month.toString()
+            setMonthView()
         }
 
         binding.nextMonth.setOnClickListener {
-            currentMonth++
-            updateMonthView()
+            // 다음 달 버튼 이벤트
+            selectedDate = selectedDate.plusMonths(1)
+            setMonth = selectedDate.month.toString()
+            setMonthView()
         }
 
         // 설정 버튼 클릭 이벤트 처리
@@ -43,9 +85,130 @@ class BudgetFragment : Fragment() {
             showBudgetSettingDialog()
         }
 
-        updateMonthView()
+        setMonthView()
+
 
         return view
+    }
+
+    private fun setMonthView() {
+        binding.nowMonth.text = monthYearFromDate(selectedDate)
+
+        // 날짜 생성해서 리스트 담기
+        val dayList = dayInMonthArray(selectedDate)
+
+        calendarFromFirebase() // 월별 정보 가져오기
+    }
+
+    private fun monthYearFromDate(date: LocalDate): String {
+        var formatter = DateTimeFormatter.ofPattern("MM월 yyyy")
+        return date.format(formatter)
+    }
+
+    private fun dayInMonthArray(date: LocalDate): ArrayList<LocalDate?> {
+        // 날짜 생성
+        var dayList = ArrayList<LocalDate?>()
+        var yearMonth = YearMonth.from(date)
+
+        // 해당 월 마지막 날짜 가져오기(28, 30, 31일)
+        var lastDay = yearMonth.lengthOfMonth()
+
+        // 해당 월 첫 번째 날짜 가져오기(예: 5월 1일)
+        var firstDay = selectedDate.withDayOfMonth(1)
+
+        // 첫 번째 날 요일 가져오기(월:1, 일:7)
+        var dayOfWeek = firstDay.dayOfWeek.value
+
+        for(i in 1..41) {
+            if(i <= dayOfWeek || i > (lastDay + dayOfWeek)) {
+                dayList.add(null)
+            }else {
+                dayList.add(LocalDate.of(selectedDate.year, selectedDate.monthValue, i - dayOfWeek))
+            }
+        }
+
+        return dayList
+    }
+
+    private fun usernameFromFirebase() {
+        myUid = FirebaseAuth.getInstance().currentUser?.uid!!
+
+        val db = Firebase.database.getReference("togyether")
+        db.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (users in dataSnapshot.children) {
+                    val name = users.child("username").value.toString()
+                    val uid = users.child("uid").value.toString()
+                    Log.i("user", name.plus(uid))
+
+                    if (myUid == uid) {
+                        binding.myName.text = name
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Error handling
+            }
+        })
+    }
+
+    private fun calendarFromFirebase() {
+        myUid = FirebaseAuth.getInstance().currentUser?.uid!!
+
+        val db = Firebase.database.getReference("togyether")
+
+        // ValueEventListener 생성
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // 데이터베이스의 값이 변경되었을 때 실행되는 로직을 여기에 작성
+                db.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        for (users in dataSnapshot.children) {
+                            val uid = users.child("uid").value.toString()
+
+                            if (myUid == uid) {
+                                when (setMonth) {
+                                    "JANUARY" -> getMonth = "1m"
+                                    "FEBRUARY" -> getMonth = "2m"
+                                    "MARCH" -> getMonth = "3m"
+                                    "APRIL" -> getMonth = "4m"
+                                    "MAY" -> getMonth = "5m"
+                                    "JUNE" -> getMonth = "6m"
+                                    "JULY" -> getMonth = "7m"
+                                    "AUGUST" -> getMonth = "8m"
+                                    "SEPTEMBER" -> getMonth = "9m"
+                                    "OCTOBER" -> getMonth = "10m"
+                                    "NOVEMBER" -> getMonth = "11m"
+                                    "DECEMBER" -> getMonth = "12m"
+                                }
+                                val spent = users.child("calendar").child("2023").child(getMonth).child("expense").value.toString()
+                                val income = users.child("calendar").child("2023").child(getMonth).child("income").value.toString()
+                                val budget = users.child("calendar").child("2023").child(getMonth).child("budget").value.toString()
+                                val sum = users.child("calendar").child("sum").value.toString()
+                                val numberFormat = NumberFormat.getInstance(Locale.getDefault())
+
+                                binding.spentMoney.text = numberFormat.format(spent.toInt())
+                                binding.incomeMoney.text = numberFormat.format(income.toInt())
+                                binding.planMoney.text = numberFormat.format(budget.toInt())
+                                binding.sumEvery.text = numberFormat.format(sum.toInt())
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Error handling
+                    }
+                })
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // 에러 처리
+            }
+        }
+
+        // ValueEventListener를 addValueEventListener로 등록하여 데이터의 변화를 감지합니다.
+        db.addValueEventListener(valueEventListener)
     }
 
     private fun showBudgetSettingDialog() {
@@ -80,49 +243,59 @@ class BudgetFragment : Fragment() {
     private fun setBudget(budget: Int) {
         binding.planMoney.text = budget.toString()
         updateExpenseRatio()
+
+        val myUid = FirebaseAuth.getInstance().currentUser?.uid!!
+        val db = Firebase.database.getReference("togyether")
+
+        // 예산 값을 Firebase의 budget 노드에 업데이트
+        db.child(myUid).child("calendar").child("2023").child(getMonth).child("budget").setValue(budget)
+            .addOnSuccessListener {
+                // 업데이트 성공 시 처리할 로직 추가
+            }
+            .addOnFailureListener {
+                // 업데이트 실패 시 처리할 로직 추가
+            }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    private fun updateMonthView() {
-        binding.nowMonth.text = "${currentMonth}월 2023"
-
-        // 예산과 지출 데이터를 가져와서 업데이트
-        budget = getBudgetForMonth(currentMonth)
-        expenses = getExpensesForMonth(currentMonth)
-
-        //binding.budgetAmount.text = budget.toString()
-        //binding.expenseAmount.text = expenses.toString()
-
-        val ratio = if (budget != 0) expenses.toFloat() / budget.toFloat() else 0.0f
-        //binding.ratioText.text = String.format("%.1f", ratio * 100) + "%"
-
-        //updateRatioBar(ratio)
-    }
-
     // 지출 비율 업데이트
     private fun updateExpenseRatio() {
-        val progressBar = binding.progressBar
-        val expenseRatio = if (budget != 0) {
-            (expenses.toFloat() / budget.toFloat() * 100).toInt()
+        calendarFromFirebase()
+        binding.incomeMoney.text = income.toString()
+
+        // 그래프 데이터 생성
+        val entries = ArrayList<BarEntry>()
+        entries.add(BarEntry(0f, budget.toFloat()))
+        entries.add(BarEntry(1f, Math.abs(income - expenses).toFloat()))
+
+        // 그래프 데이터셋 설정
+        val dataSet = BarDataSet(entries, "Expense")
+        dataSet.colors = if (budget >= (income - expenses)) {
+            // 예산이 수입-지출보다 크거나 같을 경우 파란색으로 설정
+            listOf(Color.BLUE)
         } else {
-            0
+            // 예산이 수입-지출보다 작을 경우 빨간색으로 설정
+            listOf(Color.RED)
         }
-        progressBar.progress = expenseRatio
-    }
 
-    private fun getBudgetForMonth(month: Int): Int {
-        // 해당 월에 대한 예산 데이터를 가져오는 코드 추가
-        // 예: return budgetData[month]
-        return 0
-    }
+        // 그래프 데이터셋 리스트 생성
+        val dataSets: ArrayList<IBarDataSet> = ArrayList()
+        dataSets.add(dataSet)
 
-    private fun getExpensesForMonth(month: Int): Int {
-        // 해당 월에 대한 지출 데이터를 가져오는 코드 추가
-        // 예: return expenseData[month]
-        return 0
+        // 그래프 데이터 객체 생성
+        val data = BarData(dataSets)
+
+        // 그래프 설정
+        expenseChart.data = data
+        expenseChart.setFitBars(true)
+        expenseChart.description = Description().apply {
+            text = ""
+        }
+        expenseChart.invalidate()
     }
 }
